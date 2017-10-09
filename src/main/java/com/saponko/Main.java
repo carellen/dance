3496319dfdeac933358425fa7a9b799be5b8ca44
Vladimir <saponko@gmail.com>
2017-10-09 14:12:50 +0300

    dance-7 Add Controller's method calling in main method of Main class

--- src/main/java/com/saponko/Main.java
+++ src/main/java/com/saponko/Main.java
@@ -1,5 +1,12 @@
 package com.saponko;
 
+import com.saponko.controller.Controller;
+import com.saponko.view.View;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Controller controller = new Controller(new View());
+        controller.letsDance();
+    }
 }
