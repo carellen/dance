737a06d0423493daa60494318b8a0f77ed78d3e8
Vladimir <saponko@gmail.com>
2017-10-11 13:21:59 +0300

    dance-35 Big Final Refactoring to remove all rubbish and make things more simple  - Some correction in a controller and main classes accordingly making changes.

--- src/main/java/com/saponko/Main.java
+++ src/main/java/com/saponko/Main.java
@@ -1,24 +1,27 @@
 package com.saponko;
 
 import com.saponko.controller.DancerController;
-import com.saponko.controller.StreetDancerController;
+import com.saponko.controller.DancerControllerImpl;
+import com.saponko.model.Dancer;
+import com.saponko.model.StreetDancer;
 import com.saponko.service.InputHelper;
 import com.saponko.view.ConsoleView;
 
 public class Main {
 
     public static void main(String[] args) {
-        DancerController controller = new StreetDancerController(new ConsoleView());
-        while(true) {
+        DancerController controller = new DancerControllerImpl(new ConsoleView());
+        while (true) {
             controller.showEnterMessage();
             String[] input = null;
             input = InputHelper.getInput();
             if (input == null || input.length == 0 || input[0].equals("")) {
                 break;
             }
-            controller.setDancer(InputHelper.initializeStreetDancer(input));
-            String result = controller.getStepsQuantity();
-            if (result != null) {
+            Dancer dancer = new StreetDancer();
+            if (dancer.initializeField(input)) {
+                controller.setDancer(dancer);
+                String result = controller.getStepsQuantity();
                 controller.showResultMessage(result);
             }
         }
