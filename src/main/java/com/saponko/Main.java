0a3a611d386091afdd8a7eb5b750c31688d28bf7
Vladimir <saponko@gmail.com>
2017-10-11 10:39:02 +0300

    dance-23 Refactor main method in Main class Add some methods calls, according to changes in other classes.

--- src/main/java/com/saponko/Main.java
+++ src/main/java/com/saponko/Main.java
@@ -1,12 +1,29 @@
 package com.saponko;
 
-import com.saponko.controller.Controller;
+import com.saponko.controller.DancerController;
+import com.saponko.controller.StreetDancerController;
+import com.saponko.service.StreetDancerService;
+import com.saponko.view.ConsoleView;
 import com.saponko.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
-        Controller controller = new Controller(new View());
-        controller.letsDance();
+        DancerController controller = new StreetDancerController(new ConsoleView());
+        while(true) {
+            controller.showEnterMessage();
+            String[] input = null;
+            input = StreetDancerService.getInput();
+            if (input == null || input.length == 0 || input[0].equals("")) {
+                break;
+            }
+            controller.setDancer(StreetDancerService.initializeStreetDancer(input));
+            String result = controller.getStepsQuantity();
+            if (result != null) {
+                controller.showResultMessage(result);
+            }
+        }
+        StreetDancerService.closeReader();
+        controller.showGoodbyeMessage();
     }
 }
