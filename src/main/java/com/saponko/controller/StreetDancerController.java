ab2efc12dc3671ee32559162be960ff8ede051d0
Vladimir <saponko@gmail.com>
2017-10-11 10:23:53 +0300

    dance-20 Refactor Controller class Add view methods, extract interface, add ResourceBundle

---
+++ src/main/java/com/saponko/controller/StreetDancerController.java
@@ -0,0 +1,46 @@
+package com.saponko.controller;
+
+import com.saponko.model.Dancer;
+import com.saponko.model.StreetField;
+import com.saponko.view.View;
+
+import java.util.ResourceBundle;
+
+public class StreetDancerController implements DancerController {
+
+    private Dancer dancer;
+    private View view;
+    private final static ResourceBundle BUNDLE = ResourceBundle.getBundle("messages");
+
+    public StreetDancerController(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void setDancer(Dancer dancer) {
+        this.dancer = dancer;
+    }
+
+    @Override
+    public void showEnterMessage(){
+        view.showMessage(BUNDLE.getString("hello"));
+    }
+    @Override
+    public void showGoodbyeMessage(){
+        view.showMessage(BUNDLE.getString("goodbye"));
+    }
+
+    @Override
+    public void showResultMessage(String result){
+        view.showMessage(String.format(BUNDLE.getString("result") + "\r\n\r\n", result));
+    }
+
+    @Override
+    public String getStepsQuantity() {
+        if (dancer != null) {
+            int[] stepsMap = ((StreetField)dancer.getField()).getStepsMap();
+            return String.valueOf(dancer.getSteps(stepsMap));
+        }
+        return null;
+    }
+}
