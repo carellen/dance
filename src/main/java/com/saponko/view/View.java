4c7507362419025e9f8368380114ecd4779b0a15
Vladimir <saponko@gmail.com>
2017-10-11 09:57:49 +0300

    dance-17 Refactor View class(Remove unnecessary methods and extract interface)

--- src/main/java/com/saponko/view/View.java
+++ src/main/java/com/saponko/view/View.java
@@ -1,15 +1,5 @@
 package com.saponko.view;
 
-public class View {
-    public void showEnterMessage() {
-        System.out.println("Please, input array of numbers. Use space as delimiter. To quit press enter.");
-    }
-
-    public void showGoodbyeMessage() {
-        System.out.println("Goodbye! Will glad to see you again!");
-    }
-
-    public void showResultMessage(int result) {
-        System.out.format("Dancer will perform %d steps.\r\n\r\n", result);
-    }
+public interface View {
+    void showMessage(String message);
 }
